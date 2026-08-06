493a835dbe01dd269abee027f7c5e9cad829536f
Sambodia8 <samtumilty@live.com>
2026-08-07 00:36:01 +0100

    Load Bike Quest responsive polish

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from "./App";
 import "./styles.css";
 import "./bikeQuest.css";
 import "./bikeQuestMedia.css";
+import "./bikeQuestPolish.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
