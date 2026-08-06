32511546bde51880426451fa9d4b859cafac5090
Sambodia8 <samtumilty@live.com>
2026-08-06 21:45:36 +0100

    Load Bike Quest media sizing

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles.css";
 import "./bikeQuest.css";
+import "./bikeQuestMedia.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
