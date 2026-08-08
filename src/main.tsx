299f90fb899aa6548af6ec62afd6df27efb232b4
Sambodia8 <samtumilty@live.com>
2026-08-08 01:02:26 +0100

    Start Bike Quest runtime helpers

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { startBikeQuestRuntime } from "./bikeQuestRuntime";
 import "./styles.css";
 import "./bikeQuest.css";
 import "./bikeQuestMedia.css";
 import "./bikeQuestPolish.css";
+
+startBikeQuestRuntime();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
