ad27d2a99d03e4683b32af2bb4852d91bd661ff7
Sambodia8 <samtumilty@live.com>
2026-08-09 21:49:00 +0100

    Start automatic appearance controller

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { startBikeQuestRuntime } from "./bikeQuestRuntime";
+import { loadData } from "./storage";
+import { startAppearanceController } from "./theme";
 import "./styles.css";
 import "./bikeQuest.css";
 import "./bikeQuestMedia.css";
 import "./bikeQuestPolish.css";
 import "./bikeQuestRuntime.css";
+import "./theme.css";
 
 startBikeQuestRuntime();
+startAppearanceController(() => loadData().preferences.themeMode);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
